a7d9f230cedaafa949133d173765f62cd27a4032
Iron-Wolf <lefalher-mikael@hotmail.fr>
2015-05-18 17:56:15 +0200

    ajout du fichier AddFriends.java

---
+++ ProjetTuteure/src/com/iut_velizy/projettuteure/AddFriends.java
@@ -0,0 +1,20 @@
+package com.iut_velizy.projettuteure;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class AddFriends extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        return (LinearLayout) inflater.inflate(R.layout.addfriends, container, false);
+    }
+    
+}
